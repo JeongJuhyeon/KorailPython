90a597fb6ec7adc4fd64f6b395817b45f62a53ee
jjh <j.h.jeong@outlook.com>
2019-12-06 19:27:11 +0900

    Added date/time handling, JSON handling

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ DEFAULT_TIME = '200000'
 DELAY_SEC = 0.05
 DEFAULT_DATE = '20191122'
 
+
 line_kyeongjeon = ['진주', '마산', '창원', '창원중앙', '진영', '밀양', '동대구', '김천구미', '대전', '오송', '천안아산', '광명', '서울', '행신']
 
 app = Flask(__name__)
@@ -25,7 +26,7 @@ def find_route(station1, station2):
     elif (station2_index < station1_index):
         return list(reversed(line_kyeongjeon[station2_index:station1_index + 1]))
 
-
+# Need route cause need to know intermediate stations
 def find_indirect_ticket_for_route(route, date=DEFAULT_DATE, time=DEFAULT_TIME):
     train_tuples = []
 
@@ -82,7 +83,7 @@ def get_earliest_train(train_tuples):
 def convert_train_time(train):
     return int(train.dep_date + train.dep_time)
 
-
+# Don't need route cause don't have to know intermediate stations
 def find_direct_ticket(station1, station2, date=DEFAULT_DATE, time=DEFAULT_TIME):
     try:
         train = get_earliest_train_with_seat(
@@ -99,8 +100,8 @@ def find_direct_ticket(station1, station2, date=DEFAULT_DATE, time=DEFAULT_TIME)
         return None
 
 
-def find_ticket(station1, station2):
-    direct_train = find_direct_ticket(station1, station2)
+def find_ticket(station1, station2, date=DEFAULT_DATE, time=DEFAULT_TIME):
+    direct_train = find_direct_ticket(station1, station2, date, time)
     indirect_trains = find_indirect_ticket_for_route(find_route(station1, station2))
     if direct_train and not indirect_trains:
         return direct_train
@@ -109,14 +110,29 @@ def find_ticket(station1, station2):
     else:
         return get_earliest_train([(direct_train, None), indirect_trains])
 
-
-@app.route('/ticket/<station1>/<station2>')
-def find_ticket_string(station1, station2):
+@app.route('/ticket/<station1>/<station2>/<date>/<time>')
+def ticket_get_request(station1, station2, date, time):
     app.logger.info("Ticket request received: " + station1 + "-" + station2)
-    ticket = find_ticket(station1, station2)
-    return jsonify({'tickets': str(ticket[0]) + str(ticket[1])})
+    tickets = find_ticket(station1, station2, date, time)
+    ticketResult = get_ticket_result(tickets)
+    return jsonify(ticketResult)
+
+def class_object_to_dict(train):
+    return dict(
+        (key, value)
+        for (key, value) in train.__dict__.items()
+    )
+
+def get_ticket_result(tickets):
+    if len(tickets)==1 or (len(tickets)==2 and not tickets[1]):
+        ticket_result = {'isIndirect':False, 'tickets':class_object_to_dict(tickets[0]) }
+    elif len(tickets)==2:
+        ticket_result = {'isIndirect':True, 'tickets':[class_object_to_dict(tickets[0]),class_object_to_dict(tickets[1])]}
+    elif len(tickets)==0:
+        ticket_result = {}
+    return ticket_result
 
 
 if __name__ == '__main__':
-    # print(find_ticket('동대구', '서울'))
+    print(find_ticket('동대구', '서울'))
     app.run(debug=True, host='0.0.0.0')
